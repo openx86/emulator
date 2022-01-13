2f5fc6d2b0638c11bd4e5ef944a8628606408368
Chang Wei <changwei1006@gmail.com>
2022-01-14 00:52:16 +0800

    add write ProgramRegister function

--- src/register/ProgramRegister.cpp
+++ src/register/ProgramRegister.cpp
@@ -8,10 +8,5 @@
 uint32_t ProgramRegister::IP_r = 0x0000FFF0;
 uint16_t ProgramRegister::IP() { return (uint16_t)IP_r; }
 uint32_t ProgramRegister::EIP() { return (uint32_t)IP_r; }
-void ProgramRegister::IP(uint16_t value) {
-    auto* IP_16 = (uint16_t *)(&IP_r);
-    *IP_16 = value;
-}
-void ProgramRegister::EIP(uint32_t value) {
-    IP_r = value;
-}
+void ProgramRegister::IP(uint16_t value) { auto* IP_16 = (uint16_t *)(&IP_r); *IP_16 = value; }
+void ProgramRegister::EIP(uint32_t value) { IP_r = value; }
